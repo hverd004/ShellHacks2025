2565119a33a27dfe2ec4cd7df88fd551647d1923
Felix Jarquin <145188084+FelixJ98@users.noreply.github.com>
2025-09-28 02:39:37 -0400

    first bit daemon

--- Assets/Scripts/DemonScripts/DemonSceneScript.cs
+++ Assets/Scripts/DemonScripts/DemonSceneScript.cs
@@ -13,37 +13,46 @@ public class DemonSceneScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        runsub = StartCoroutine(runSubtitles());
+        runsub = StartCoroutine(DemonPie());
     }
     IEnumerator runSubtitles()
     {
+
         yield return new WaitForSeconds(2);
         audiosource.clip = audioclips[0];
         audiosource.Play();
         textfield.text = "This is it!! The Final Act!! The Great Climax.";
+        yield return new WaitForSeconds(4f);
         textfield.text = "The demon lord is right in front of you";
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(2.2f);
+        textfield.text = "you’re officially one move away from victory and fame.";
+        yield return new WaitForSeconds(3.3f);
+        //buffer between two voice files
+        yield return new WaitForSeconds(2f);
+        audiosource.clip = audioclips[1];
+        audiosource.Play();
         textfield.text = "Another foolish hero at my door, another life thrown away trying to oppose me.";
-        yield return new WaitForSeconds(1.2f);
+        yield return new WaitForSeconds(5f);
         textfield.text = " I will admit, it’s impressive you’ve made it this far.";
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(4f);
         textfield.text = "Very few have walked these halls with the intent to kill me.";
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(4.4f);
         textfield.text = "For your accomplishment I will offer you this mercy.";
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(4f);
         textfield.text = "Join me,";
         yield return new WaitForSeconds(1.2f);
         textfield.text = "and you will be part of something greater";
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3.3f);
         textfield.text = "Oppose me, and you’ll die in vain like all the others.";
-        yield return new WaitForSeconds(15);
+        yield return new WaitForSeconds(6);
         textfield.text = "";
+        
     }
 
     public IEnumerator DemonPie()
     {
         yield return new WaitForSeconds(2);
-        audiosource.clip = audioclips[0];
+        audiosource.clip = audioclips[2];
         audiosource.Play();
         textfield.text = "A pie? Are we serious? You came all this way just to give me a pie.";
         yield return new WaitForSeconds(2.5f);
